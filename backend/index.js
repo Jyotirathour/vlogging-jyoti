e6ae420b62f3ad8cf38fc116ea1aeb335bab99de
Jyotirathour <jrathorejyoti@gmail.com>
2021-12-19 19:31:08 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const cors = require ("cors");
+
+const port = 5000;
+
+const userRouter = require("./routers/userRouter");
+
+app.use(cors({ origin: ["http://localhost:3000"] }));
+app.use(express.json());
+
+app.use(express.static("./uploads"));
+
+app.use("/user", userRouter);
+
+app.listen(port, () => {
+  console.log("server successfully started on port 5000");
+});
