7228739ada482ca26007f10e3baea41600bd6f2d
m-triple-m <triplem656@gmail.com>
2021-12-19 20:06:57 +0530

    ..

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -1,38 +1,58 @@
+import "./home.css";
+
 const Home = () => {
-  const name = "Leon S Kennedy";
-
-  // You can also store JSX syntax in variable to display
-  const formula = (
-    <h3>
-      H<sub>2</sub>0
-    </h3>
-  );
-
-  // You can call JS functions inside JSX using {}
-  const sum = (a, b) => {
-    return a + b;
-  };
-
-  // !!!!!!! Object cannot be displayed directly in JSX
-  // You have to put individual key to display it
-  const userdetails = {
-    name: "Leon S Kennedy",
-    email: "leon@mail.com",
-    age: 23,
-  };
-
   return (
-    <div>
-      <h1>Home Component</h1>
-      <h2>{name}</h2>
-      {formula}
-      {sum(34, 345)}
-      <br />
-      {/* not valid */}
-      {/* {userdetails} */}
-      <h2>Name : {userdetails.name}</h2>
-      <h2>Email : {userdetails.email}</h2>
-      <h2>Age : {userdetails.age}</h2>
+    <div class="container">
+      <div class="navbar">
+        <div class="icon">
+          <h2 class="logo">VloG</h2>
+        </div>
+        <div class="menu">
+          <ul>
+            <li>
+              <a herf="#">HOME</a>
+            </li>
+            <li>
+              <a herf="#">ABOUT</a>
+            </li>
+            <li>
+              <a herf="#">INFO</a>
+            </li>
+            <li>
+              <a herf="#">COMMUNITY</a>
+            </li>
+            <li>
+              <a herf="#">CONTACT</a>
+            </li>
+          </ul>
+        </div>
+        <div class="btn">
+          <button class="btn-1">Login</button>
+          <button class="btn-2">Signup</button>
+        </div>
+      </div>
+      <div class="content">
+        <h1>
+          VIDEO
+          <br />
+          <span>Blogging</span>
+        </h1>
+        <p class="para">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit.Libero <br />{" "}
+          dolor reiciendis facilis sit voluptas distinctio quia odio illum{" "}
+          <br /> dolore! Adipisci eveniet distinctio optio doloremque <br />
+          saepe perferendis deleniti accusamus architecto quis
+          <br />.
+        </p>
+        <button class="btn1">Get Started</button>
+        <button class="btn2">Free demo</button>
+        <div class="img">
+          <img
+            src="https://cdn.dribbble.com/users/3639405/screenshots/9685335/media/dc97db3a11721dcd8241cfe564b6ec30.jpg"
+            alt=""
+          />
+        </div>
+      </div>
     </div>
   );
 };
